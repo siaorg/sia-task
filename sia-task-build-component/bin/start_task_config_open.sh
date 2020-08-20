ff5dfdf67b59f54a5c44122332baafa5832adb0b
nuccch2010 <nuccch2010@163.com>
2020-08-20 21:23:52 +0800

    fix work path check

--- sia-task-build-component/bin/start_task_config_open.sh
+++ sia-task-build-component/bin/start_task_config_open.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-current_dir=$(pwd)
-cd $current_dir/bin
+case "`uname`" in
+	Linux)
+		bin_absolute_path=$(readlink -f $(dirname $0))
+		;;
+	*)
+		bin_absolute_path=`cd $(dirname $0);pwd`
+		;;
+esac
+
+export LANG=en_US.UTF-8
+base_dir=${bin_absolute_path}/..
+cd $base_dir/bin
 nohup sh run4config.sh task_config_open sia-task-config-1.0.0.jar >$(pwd)/task_config_open.start 2>&1 &
