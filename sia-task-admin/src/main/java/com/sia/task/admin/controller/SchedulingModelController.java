92b8f389a6c78977faac8b402c9ac662c56e6173
zhengweimao <470229170@qq.com>
2020-09-04 18:36:08 +0800

    push code: sia-task-admin fix api

--- sia-task-admin/src/main/java/com/sia/task/admin/controller/SchedulingModelController.java
+++ sia-task-admin/src/main/java/com/sia/task/admin/controller/SchedulingModelController.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,13 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+/**
+ * 调度模型计算服务
+ *
+ * @author maozhengwei
+ * @version V1.0.0
+ * @date 2020/9/4 6:35 下午
+ **/
 @Slf4j
 @RestController
 @RequestMapping("/schedulingmodel")
