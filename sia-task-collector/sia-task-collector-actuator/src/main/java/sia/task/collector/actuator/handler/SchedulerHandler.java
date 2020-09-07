f824a99660f36d22b17f2326f7528401ba282da3
zhengweimao <470229170@qq.com>
2020-09-07 16:30:55 +0800

    修改 SchedulerHandler 剔除注解

--- sia-task-collector/sia-task-collector-actuator/src/main/java/sia/task/collector/actuator/handler/SchedulerHandler.java
+++ sia-task-collector/sia-task-collector-actuator/src/main/java/sia/task/collector/actuator/handler/SchedulerHandler.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,22 +20,21 @@
 
 package sia.task.collector.actuator.handler;
 
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import sia.task.collector.actuator.entity.SystemInfo;
 
 /**
  * 调度器资源监听处理器
  *
- * @see
  * @author maozhengwei
- * @date 2020/6/1 10:13 上午
  * @version V1.0.0
+ * @date 2020/6/1 10:13 上午
+ * @see
  **/
-@RestController
+//@RestController
 public class SchedulerHandler {
 
     public Flux<SystemInfo> getSystemInfo() {
-return null;
+        return null;
     }
 }
